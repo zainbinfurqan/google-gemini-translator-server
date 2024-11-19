3942b2815989ba983d7b6756a8be0654b0316c65
zain ahmed <zain.ahmed199524@gmail.com>
2024-11-19 22:41:01 +0800

    update keys

---
+++ functions/hello.js
@@ -0,0 +1,7 @@
+// functions/hello.js
+exports.handler = async (event, context) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello from Netlify!" }),
+    };
+  };
